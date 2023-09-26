8fe8a54724a82a60fe042ae97088e95b02a6144e
Roy <vargas.sap321@gmail.com>
2023-09-25 22:16:00 -0700

    feat: Add new endpoint in /api/v1/stats where return a numbers of models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """Module to import app_views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 # Define a route on the app_views blueprint
@@ -9,3 +10,12 @@ from flask import jsonify
 def get_status():
     status = {"status": "OK"}
     return jsonify(status)
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def count():
+    objects = storage.all()
+    dict = {}
+    for k, v in objects.items():
+        num_objs[k] = storage.count(v)
+    return jsonify(dict)
