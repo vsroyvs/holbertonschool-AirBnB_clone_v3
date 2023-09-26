74f3b024c204dc4040879d8772bcbe07cff05598
Roy <vargas.sap321@gmail.com>
2023-09-25 21:09:29 -0700

    fix: PEP8 error blank line at end of file

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,6 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-from flask_cors import CORS
 
 
 @app.teardown_appcontext
@@ -17,8 +16,6 @@ def close(self):
     storage.close()
 
 if __name__ == '__main__':
-    h = "0.0.0.0"
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else h
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
-    app.run(host=host, port=port, threaded=True)
-  
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)  
