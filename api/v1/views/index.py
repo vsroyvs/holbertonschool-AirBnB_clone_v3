f6301bff52b86e718531302114dcd5ffb9ebfa21
Roy <vargas.sap321@gmail.com>
2023-09-25 21:05:03 -0700

    fix: PEP8 Error expect 2 blank lines after importation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""""""
+"""Module to import app_views"""
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
 
 
-"""Define a route on the app_views blueprint"""
+# Define a route on the app_views blueprint
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     status = {"status": "OK"}
