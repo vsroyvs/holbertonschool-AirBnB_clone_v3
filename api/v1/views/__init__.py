f3673c743c9317e1dea222d59fe15a0cbebd09a7
yelsy <yelsycg17@gmail.com>
2023-09-25 21:49:07 -0500

    feat: initialization module for organize app

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""initialization """
+from flask import Blueprint
+
+
+"""Create a instance and define a route within the Blueprint"""
+app_views = Blueprint("app_views",__name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
